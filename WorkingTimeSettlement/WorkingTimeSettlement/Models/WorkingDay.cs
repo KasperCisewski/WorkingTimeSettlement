3f23f1c2c410970127a36aef55b24b8eae6efe46
Kasper Cisewski <kaspercisewski@gmail.com>
2018-09-19 01:55:33 +0200

    add model and pages

---
+++ WorkingTimeSettlement/WorkingTimeSettlement/Models/WorkingDay.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WorkingTimeSettlement.Models
+{
+    class WorkingDay
+    {
+        public DateTime Date { get; set; }
+
+    }
+}
